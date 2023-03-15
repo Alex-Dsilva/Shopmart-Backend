0471233d8df7b98612ef79d59b54fc86873c3c43
Alex Dislva <alexdislva79@gmail.com>
2023-03-15 23:51:29 +0530

    updated Questions

--- Routes/question.router.js
+++ Routes/question.router.js
@@ -8,5 +8,38 @@ questionRouter.get('/products/:productId/questions', async (req, res) => {
     .catch(error => next(error));
 });
 
+questionRouter.post('/products/:productId/questions', (req, res) => {
+  const { questionText } = req.body;
+  const newQuestion = new Question({
+    questionText,
+    productId: req.params.productId,
+    answeredByAdmin: false, // Set to false by default
+  });
+  newQuestion
+    .save()
+    .then((savedQuestion) => {
+      res.status(201).json({ question: savedQuestion });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+questionRouter.put('/questions/:questionId', (req, res) => {
+  const { answer } = req.body;
+  const { questionId } = req.params;
+  Question.findByIdAndUpdate(
+    questionId,
+    { answer: answer, isAnswered: true },
+    { new: true },
+    (err, updatedQuestion) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+      } else {
+        res.status(200).json({ question: updatedQuestion });
+      }
+    }
+  );
+});
 
 module.exports = { questionRouter };
